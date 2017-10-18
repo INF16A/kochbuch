8a00609c805379dec56695022868a80521e22349
alexkrieg <alexkrieg93@googlemail.com>
2017-10-18 11:52:35 +0200

    Erstellung Kommentar-Service v1
    Kommentar-Model, -Repository, -Controller usw.

---
+++ src/main/java/de/dhbw/mosbach/inf16a/kochbuch/commentservice/CommentController.java
@@ -0,0 +1,40 @@
+package de.dhbw.mosbach.inf16a.kochbuch.commentservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @author 💩 Alexander Krieg
+ */
+
+@RestController
+public class CommentController
+{
+
+	@Autowired
+	private CommentRepository commentRepository;
+
+
+	@GetMapping(value = "/comments")
+	public List<Comment> comments()
+	{
+		return commentRepository.findAllByOrderByCreationDateDesc();
+	}
+
+	@GetMapping(value = "/comments/{recipeID}")
+	public List<Comment> commentsForRecipe(@PathVariable(value = "recipeID") long recipeID)
+	{
+		return commentRepository.findByRecipeIDByOrderByCreationDateDesc(recipeID);
+	}
+
+	@PostMapping(value = "/comment")
+	public Comment addComment(@RequestBody Comment theNewComment)
+	{
+		return this.commentRepository.save(theNewComment);
+		// return commentRepository.findAllByOrderByCreationDateDesc();
+	}
+
+	
+}
---
+++ src/main/java/de/dhbw/mosbach/inf16a/kochbuch/commentservice/CommentRepository.java
@@ -0,0 +1,16 @@
+package de.dhbw.mosbach.inf16a.kochbuch.commentservice;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+/**
+ * @author Alexander Krieg
+ */
+
+public interface CommentRepository extends CrudRepository<Comment, Long>
+{
+	List<Comment> findByUserIDOrderByCreationDateDesc(long userID);
+	List<Comment> findByRecipeIDOrderByCreationDateDesc(long recipeID);
+	List<Comment> findAllByOrderByCreationDateDesc();
+}
