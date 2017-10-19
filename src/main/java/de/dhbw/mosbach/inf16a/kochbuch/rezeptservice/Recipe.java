08f43b06ad1896706dbe594702b682a5de95472d
Hörner, Thomas <tho@mainmetall.de>
2017-10-19 11:35:40 +0100

    test

--- src/main/java/de/dhbw/mosbach/inf16a/kochbuch/rezeptservice/Recipe.java
+++ src/main/java/de/dhbw/mosbach/inf16a/kochbuch/rezeptservice/Recipe.java
@@ -1,11 +1,17 @@
 package de.dhbw.mosbach.inf16a.kochbuch.rezeptservice;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -25,7 +31,7 @@ public class Recipe {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;
+	private Long id;
 
 	@NonNull
 	private String name;
@@ -34,9 +40,22 @@ public class Recipe {
 	private String description;
 
 	@NonNull
-	private int difficulty;
+	private Integer difficulty;
 
 	@NonNull
 	private Date createDate;
 
+//	@NonNull
+//	@ManyToMany(cascade = CascadeType.ALL)
+//	@JoinTable(joinColumns = @JoinColumn(name = "recipeId"), inverseJoinColumns = @JoinColumn(name = "tagId"))
+//	private List<Tag> tags;
+
+	public void demodata() {
+		this.name = "hi";
+		this.description = "hides";
+		this.difficulty = 5;
+		this.createDate = new Date();
+//		List<Tag> tagss = Tag.demo();
+//		this.tags = tagss;
+	}
 }
