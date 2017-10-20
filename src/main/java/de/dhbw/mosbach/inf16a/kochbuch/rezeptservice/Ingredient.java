38ee0386ac410d9f9ed19a3baa00b5dc3f2b5e6e
Berberich André <Berbs1989@gmail.com>
2017-10-20 09:12:34 +0200

    Expandend imports.

--- src/main/java/de/dhbw/mosbach/inf16a/kochbuch/rezeptservice/Ingredient.java
+++ src/main/java/de/dhbw/mosbach/inf16a/kochbuch/rezeptservice/Ingredient.java
@@ -1,11 +1,19 @@
 package de.dhbw.mosbach.inf16a.kochbuch.rezeptservice;
 
-import javax.persistence.*;
+
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 /**
  * 
