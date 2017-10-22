ec1bfb95c9ba730dc3602f8c106c66ced5bd68e9
Berberich André <berbs1989@googlemail.com>
2017-10-22 12:15:02 +0200

    Added get methods for ingredient controller

--- src/main/java/de/dhbw/mosbach/inf16a/kochbuch/ingredientservice/IngredientController.java
+++ src/main/java/de/dhbw/mosbach/inf16a/kochbuch/ingredientservice/IngredientController.java
@@ -1,6 +1,11 @@
 package de.dhbw.mosbach.inf16a.kochbuch.ingredientservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author André Berberich
@@ -8,5 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IngredientController {
 
+    @Autowired
     private IngredientRepository ingredientRepository;
+
+    @GetMapping(value = "/ingredients")
+    public List<Ingredient> ingredients()
+    {
+        return ingredientRepository.findAll();
+    }
+
+    @GetMapping(value="/ingredient/{id}")
+    public Ingredient getIngredientById(@PathVariable(value = "id")Long id) {
+        return ingredientRepository.findFirstById(id);
+    }
 }
