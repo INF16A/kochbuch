554f7f2a59dd6c94cfacbf9fef8778701bba1a13
Robert Zebec <Robert.Zebec@tecalliance.net>
2017-10-22 20:18:43 +0200

    *Added get method to the RecipeIngredient Controller

--- src/main/java/de/dhbw/mosbach/inf16a/kochbuch/ingredientservice/RecipeIngredientController.java
+++ src/main/java/de/dhbw/mosbach/inf16a/kochbuch/ingredientservice/RecipeIngredientController.java
@@ -1,12 +1,28 @@
 package de.dhbw.mosbach.inf16a.kochbuch.ingredientservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author Robert Zebec
  */
 @RestController
 public class RecipeIngredientController {
-
+	@Autowired
     private RecipeIngredientRepository recipeIngredientRepository;
+
+	@GetMapping(value = "/recipeIngredient/{id}")
+	public RecipeIngredient getRecipeIngredientById(@PathVariable(value = "id") long id) {
+		return recipeIngredientRepository.findFirstById(id);
+	}
+
+	@GetMapping(value = "/recipeIngredients")
+	public List<RecipeIngredient> recipeIngredients()
+	{
+		return recipeIngredientRepository.findAll();
+	}
 }
--- src/main/java/de/dhbw/mosbach/inf16a/kochbuch/ingredientservice/RecipeIngredientRepository.java
+++ src/main/java/de/dhbw/mosbach/inf16a/kochbuch/ingredientservice/RecipeIngredientRepository.java
@@ -2,9 +2,13 @@ package de.dhbw.mosbach.inf16a.kochbuch.ingredientservice;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 /**
  * @author Robert Zebec
  */
 
 public interface RecipeIngredientRepository extends CrudRepository<RecipeIngredient, Long> {
+    RecipeIngredient findFirstById(long id);
+    List<RecipeIngredient> findAll();
 }
