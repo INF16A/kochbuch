e6f6b1d27e3de3e4f95e4bd4682cf1e4edd3749a
Patrick Hahn <patricksilashahn@gmx.net>
2017-10-24 00:34:44 +0200

    basic spring security setup with everything allowed

--- src/main/java/de/dhbw/mosbach/inf16a/kochbuch/Main.java
+++ src/main/java/de/dhbw/mosbach/inf16a/kochbuch/Main.java
@@ -2,6 +2,11 @@ package de.dhbw.mosbach.inf16a.kochbuch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * @author Patrick Hahn
@@ -14,5 +19,20 @@ public class Main
 	public static void main(String[] args)
 	{
 		SpringApplication.run(Main.class, args);
+	}
+
+	@Configuration
+	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter
+	{
+		@Override
+		protected void configure(HttpSecurity http) throws Exception
+		{
+			http
+					.formLogin()
+					.and()
+					.authorizeRequests()
+					.anyRequest().permitAll();
+		}
 	}
 }
