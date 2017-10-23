b8c2e3c7f40d8bf0f868454a196507afd55fe0b6
MakroCow <yoco.lie@gmail.com>
2017-10-23 21:59:26 +0200

    Added a function, that returns all recipes with a certain tag

---
+++ src/main/java/de/dhbw/mosbach/inf16a/kochbuch/rezeptservice/RezeptListenController.java
@@ -0,0 +1,35 @@
+package de.dhbw.mosbach.inf16a.kochbuch.rezeptservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class RezeptListenController {
+
+    @Autowired
+    private RecipeRepository recipeRepository;
+
+    @RequestMapping("/getRecipesbyTag")
+
+    @GetMapping(value = "/recipes/{tag}")
+    public List<Recipe> getRecipesByTag(String tag) {
+        List hilfsarray = recipeRepository.findAll();    //Alle Rezepte ins Hilfsarray reinspeichern
+        Iterator<Recipe> iter = hilfsarray.iterator();
+        while (iter.hasNext()) {
+            Recipe rep = iter.next();
+            if(! rep.getTags().contains(tag)){
+                hilfsarray.remove(rep);
+            }
+        }
+        return hilfsarray;
+    }
+
+    @GetMapping(value="/recipe/{id}")
+    public Recipe getRecipeById(Long id) {
+        return recipeRepository.findOne(id);
+    }
+}
