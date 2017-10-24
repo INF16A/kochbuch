a2b0e112ba674c262b2ab8c257081b87fb4f528e
Patrick Hahn <patricksilashahn@gmx.net>
2017-10-24 15:13:11 +0200

    fixed sql error

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,2 @@
 -- @author Patrick Hahn
-INSERT IGNORE INTO user (id, username, password)
-VALUES (1, 'admin', '$2a$10$cJIF6ZmL9AjqqwV2W.16MeNK6brOQ2P4ub7jBq/50xtSWiyfOFJlW')
+INSERT IGNORE INTO user (id, username, password) VALUES (1, 'admin', '$2a$10$cJIF6ZmL9AjqqwV2W.16MeNK6brOQ2P4ub7jBq/50xtSWiyfOFJlW')
