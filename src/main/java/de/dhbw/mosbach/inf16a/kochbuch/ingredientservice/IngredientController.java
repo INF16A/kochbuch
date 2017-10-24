c5607304f766f8ff19625c4ead73e28f4a71fd78
Berberich André <berbs1989@googlemail.com>
2017-10-24 16:12:57 +0200

    Added a (test) post method to post ingredient

--- src/main/java/de/dhbw/mosbach/inf16a/kochbuch/ingredientservice/IngredientController.java
+++ src/main/java/de/dhbw/mosbach/inf16a/kochbuch/ingredientservice/IngredientController.java
@@ -1,9 +1,7 @@
 package de.dhbw.mosbach.inf16a.kochbuch.ingredientservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +23,11 @@ public class IngredientController {
     @GetMapping(value="/ingredient/{id}")
     public Ingredient getIngredientById(@PathVariable(value = "id")Long id) {
         return ingredientRepository.findFirstById(id);
+    }
+
+    @PostMapping(value = "/ingredient")
+    public Ingredient addIngredient(@ModelAttribute Ingredient newIngredient)
+    {
+        return this.ingredientRepository.save(newIngredient);
     }
 }
