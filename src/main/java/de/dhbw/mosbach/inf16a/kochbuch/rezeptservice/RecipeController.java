95137fa72245ec74272ae50a6904b00bc54b53ef
theresa2403 <theresa.reus@web.de>
2017-10-26 17:31:27 +0200

    Remove of JsonIgnore in RecipeIngredient and changed Request Variable to PathVariable in RecipeController

--- src/main/java/de/dhbw/mosbach/inf16a/kochbuch/rezeptservice/RecipeController.java
+++ src/main/java/de/dhbw/mosbach/inf16a/kochbuch/rezeptservice/RecipeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -22,7 +23,7 @@ public class RecipeController {
 	}
 
 	@GetMapping(value = "/recipe/{id}")
-	public Recipe getRecipeById(Long id) {
+	public Recipe getRecipeById(@PathVariable(value="id") long id) {
 		return recipeRepository.findOne(id);
 	}
 
