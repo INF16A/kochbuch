9af5c47f5685bc0b56aeb8cdc7b4fd3f3391151b
Thomas Hörner <hoernerth@gmail.com>
2017-11-02 17:31:36 +0100

    unit controller, misc stuff

--- src/main/java/de/dhbw/mosbach/inf16a/kochbuch/tags/TagController.java
+++ src/main/java/de/dhbw/mosbach/inf16a/kochbuch/tags/TagController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +24,7 @@ public class TagController {
 	@Autowired
 	private TagRepository tagRepository;
 
+	@CrossOrigin
 	@GetMapping(value = "/tag")
 	public List<Tag> searchTagByName(@RequestParam("search") String name) {
 		if (name == null || name.length() == 0) {
