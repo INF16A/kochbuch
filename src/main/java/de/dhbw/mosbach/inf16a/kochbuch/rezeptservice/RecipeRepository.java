f811631956af6288e3df11e8d3f639ac637b702d
MakroCow <yoco.lie@gmail.com>
2017-11-04 00:20:44 +0100

    Revised the criticized part

--- src/main/java/de/dhbw/mosbach/inf16a/kochbuch/rezeptservice/RecipeRepository.java
+++ src/main/java/de/dhbw/mosbach/inf16a/kochbuch/rezeptservice/RecipeRepository.java
@@ -6,4 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
 	List <Recipe> findAll();
+	List <Recipe> findByNameContaining(String name);
+	List <Recipe> findByTags_NameContaining(String tag);
+
 }
--- src/main/java/de/dhbw/mosbach/inf16a/kochbuch/rezeptservice/RezeptListenController.java
+++ src/main/java/de/dhbw/mosbach/inf16a/kochbuch/rezeptservice/RezeptListenController.java
@@ -1,9 +1,7 @@
 package de.dhbw.mosbach.inf16a.kochbuch.rezeptservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -13,24 +11,23 @@ import java.util.List;
  * @author Team Chrocorg: Christian Werner, Yoco Harrmann und Georg Frey
  */
 
+
 @RestController
 public class RezeptListenController {
 
     @Autowired
     private RecipeRepository recipeRepository;
 
-    // @RequestMapping("/getRecipesbyTag")
+    @CrossOrigin
+    @GetMapping(value = "/recipes/{name}")
+    public List<Recipe> getRecipesByName(String name) {
+        return recipeRepository.findByNameContaining(name);
+    }
 
+    @CrossOrigin
     @GetMapping(value = "/recipes/{tag}")
     public List<Recipe> getRecipesByTag(String tag) {
-        List hilfsarray = recipeRepository.findAll();    //Alle Rezepte ins Hilfsarray reinspeichern
-        Iterator<Recipe> iter = hilfsarray.iterator();   //Alle nichtpassenden Rezepte wieder rauswerfen
-        while (iter.hasNext()) {
-            Recipe rep = iter.next();
-            if(! rep.getTags().contains(tag)){
-                hilfsarray.remove(rep);
-            }
-        }
-        return hilfsarray;
+        return recipeRepository.findByTags_NameContaining(tag);
     }
+
 }
