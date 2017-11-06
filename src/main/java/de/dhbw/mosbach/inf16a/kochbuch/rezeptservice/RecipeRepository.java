35dd2dde1e941784413ead6208e49cea9ce31899
squanchy11 <32268535+squanchy11@users.noreply.github.com>
2017-11-06 11:18:12 +0100

    Added ListRecipeByIngredient und ListRecipeByUser

--- src/main/java/de/dhbw/mosbach/inf16a/kochbuch/rezeptservice/RecipeRepository.java
+++ src/main/java/de/dhbw/mosbach/inf16a/kochbuch/rezeptservice/RecipeRepository.java
@@ -4,9 +4,15 @@ import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * @author Theresa Reus, Philipp Steigler, Roman Würtemberger und Jarno Wagner
+ */
+
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
 	
 	List <Recipe> findAll();
 	List <Recipe> findByNameContaining(String name);
 	List <Recipe> findByTags_NameContaining(String tag);
+	List<Recipe> getRecipesByIngredient(String ingredient);
+	List<Recipe> getRecipesByUser(String user);
 }
