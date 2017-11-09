2084aec3967b66d943f251513d067c9d690de966
MakroCow <yoco.lie@gmail.com>
2017-11-09 19:24:33 +0100

    Added methods for searching page

--- src/main/java/de/dhbw/mosbach/inf16a/kochbuch/rezeptservice/RecipeRepository.java
+++ src/main/java/de/dhbw/mosbach/inf16a/kochbuch/rezeptservice/RecipeRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.repository.CrudRepository;
  * @author Thomas Hörner
  * @author Enrico Greßer
  * @author Florian Eder
+ * @author Yoco Harrmann
  */
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
 
@@ -15,7 +16,15 @@ public interface RecipeRepository extends CrudRepository<Recipe, Long> {
 
     Recipe save(Recipe recipe);
 
+
+
+    /* Yoco Harrmann */
 	List <Recipe> findByNameContaining(String name);
 
 	List <Recipe> findByTags_NameContaining(String tag);
+
+    List <Recipe> findByIngredients_NameContaining(String tag);
+
+    // List <Recipe> findByUsernameContaining(String tag);
+    /* Ende Yoco */
 }
--- src/main/java/de/dhbw/mosbach/inf16a/kochbuch/rezeptservice/RezeptListenController.java
+++ src/main/java/de/dhbw/mosbach/inf16a/kochbuch/rezeptservice/RezeptListenController.java
@@ -7,27 +7,42 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
-/**
- * @author Team Chrocorg: Christian Werner, Yoco Harrmann und Georg Frey
+/** @author Team Chrocorg: Christian Werner, Yoco Harrmann und Georg Frey
+ *  @author Jarno Wagner, Philipp Steigler, Roman Würtemberger, Yoco Harrmann
  */
 
+/** Anfang Christian Werner, Yoco Harrmann, Georg Frey */
+    @RestController
+    public class RezeptListenController {
 
-@RestController
-public class RezeptListenController {
+        @Autowired
+        private RecipeRepository recipeRepository;
 
-    @Autowired
-    private RecipeRepository recipeRepository;
+        @CrossOrigin
+        @GetMapping(value = "/recipes/{name}")
+        public List<Recipe> getRecipesByName(String name) {
+            return recipeRepository.findByNameContaining(name);
+        }
 
-    @CrossOrigin
-    @GetMapping(value = "/recipes/{name}")
-    public List<Recipe> getRecipesByName(String name) {
-        return recipeRepository.findByNameContaining(name);
-    }
+        @CrossOrigin
+        @GetMapping(value = "/recipes/{tag}")
+        public List<Recipe> getRecipesByTag(String tag) {
+            return recipeRepository.findByTags_NameContaining(tag);
+        }
+    /* Ende Christian, Yoco, Georg */
 
-    @CrossOrigin
-    @GetMapping(value = "/recipes/{tag}")
-    public List<Recipe> getRecipesByTag(String tag) {
-        return recipeRepository.findByTags_NameContaining(tag);
-    }
 
+    /* Anfang Jarno, Philipp, Roman, Yoco */
+        @CrossOrigin
+        @GetMapping(value = "/recipes/{user}")
+        public List<Recipe> getRecipesByUser(String user) {
+            return recipeRepository.findByNameContaining(user);
+        }
+
+        @CrossOrigin
+        @GetMapping(value = "/recipes/{ingredient}")
+        public List<Recipe> getRecipesByIngredient(String ingredient) {
+            return recipeRepository.findByTags_NameContaining(ingredient);
+        }
+    /* Ende Jarno, Philipp, Roman, Yoco */
 }
